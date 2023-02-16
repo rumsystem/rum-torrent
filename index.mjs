9a18a783fbd9be7f636e4402ff7684862e25a130
Leask Wong <i@leaskh.com>
2023-02-16 18:33:22 -0500

    adding deps

--- index.mjs
+++ index.mjs
@@ -1,5 +1,10 @@
 import _ from './lib/horizon.mjs'
+import * as color from './lib/color.mjs';
+import * as config from './lib/config.mjs';
+import * as encryption from './lib/encryption.mjs';
+import * as storage from './lib/storage.mjs';
 import * as torrent from './lib/torrent.mjs';
 import * as tracker from './lib/tracker.mjs';
+import * as utilitas from './lib/utilitas.mjs';
 
-export { torrent, tracker };
+export { color, config, encryption, storage, torrent, tracker, utilitas };
