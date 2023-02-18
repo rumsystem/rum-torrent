0929381a1c8466e9386c11a9f5cdb90eb492af66
Leask Wong <i@leaskh.com>
2023-02-17 22:01:52 -0500

    Frozen @ Fri Feb 17 22:01:52 EST 2023

--- patch.sh
+++ patch.sh
@@ -1,2 +1,2 @@
 cp patches/parse-http.js node_modules/bittorrent-tracker/lib/server/parse-http.js
-cp patches/index.js node_modules/bittorrent-peerid/index.js
+# cp patches/index.js node_modules/bittorrent-peerid/index.js
