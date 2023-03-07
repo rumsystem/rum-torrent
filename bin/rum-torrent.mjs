4850408ab46cecae0d43194d694a4bd568000217
Leask Wong <i@leaskh.com>
2023-03-06 19:38:42 -0500

    fixed for engine version lower than v19

--- bin/rum-torrent.mjs
+++ bin/rum-torrent.mjs
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 
 import { dashboard, torrent } from '../index.mjs';
-import { parseArgs } from 'node:util';
 import { utilitas } from 'utilitas';
 
 const log = content => utilitas.log(content, import.meta.url);
 
-// https://kgrz.io/node-has-native-arg-parsing.html
-const { values: args } = parseArgs({
-    options: {
-        seed: {
-            type: 'string',
-            short: 's',
-            default: '',
-        }
+const args = await (async (options) => {
+    const { parseArgs } = await import('node:util');
+    if (parseArgs) { // https://kgrz.io/node-has-native-arg-parsing.html
+        const { values } = parseArgs({
+            options: { seed: { type: 'string', short: 's', default: '' } },
+            ...options || {},
+        });
+        return values;
     }
-});
+    let args = {}; // simple fallback for node version < 19
+    process.argv.map(arg => {
+        const item = arg.replace(/^\-*([^=]*)=(.*)$/ig, '$1<,>$2').split('<,>');
+        item.length > 1 && (args[item[0]] = item[1]);
+    });
+    return args;
+})();
 
 try {
     assert(args.seed, 'Please provide a file to seed or torrent-info(.torrent or magnet link) to download.', 400);
