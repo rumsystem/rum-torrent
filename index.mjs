e2fc67147181861d28d027406425ddda4593548a
Leask Wong <i@leaskh.com>
2023-07-08 23:11:18 -0400

    merged

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
+import * as auth from './lib/auth.mjs';
 import * as config from './lib/config.mjs';
+import * as crypto from './lib/crypto.mjs';
 import * as dashboard from './lib/dashboard.mjs';
 import * as natUpnp from './lib/natUpnp.mjs';
 import * as torrent from './lib/torrent.mjs';
 import * as tracker from './lib/tracker.mjs';
 
-export { config, dashboard, natUpnp, torrent, tracker };
+export { auth, config, crypto, dashboard, natUpnp, torrent, tracker };
